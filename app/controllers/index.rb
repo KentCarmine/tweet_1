a85d97c21c98f058a0db435276cbdc38457f8cec
Apprentice <apprentice@dbc-chicago031.local>
2013-11-11 11:28:57 -0600

    Added caching

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,25 +8,25 @@ post '/' do
   twitter_handle = params[:twitter_username]
   user = TwitterUser.find_or_create_by_name(:name => twitter_handle)
 
-  users_tweets = Twitter.user_timeline(twitter_handle)[0..9]
+  if user.tweets_stale?
+    puts "TWEETS ARE STALE"
+    users_tweets = Twitter.user_timeline(twitter_handle)[0..9]
 
-  # tweet_text = []
+    users_tweets.each do |tweet|
+      user.tweets.find_or_create_by_tweet_created_at(:text => tweet[:attrs][:text], :tweet_created_at => tweet[:attrs][:created_at])
+    end
 
-  users_tweets.each do |tweet|
-    # tweet_text << tweet[:attrs][:text]
-    user.tweets.find_or_create_by_tweet_created_at(:text => tweet[:attrs][:text], :tweet_created_at => tweet[:attrs][:created_at])
-    # puts tweet[:attrs][:text]
-    # puts
+    user.updated_at = Time.now
+    user.save
   end
 
-
-   redirect to "/#{twitter_handle}"
+  redirect to "/#{twitter_handle}"
 end
 
 # Display last 10 tweets of username
 get '/:username' do
   @user = TwitterUser.find_by_name(params[:username])
-  @tweets = @user.tweets.limit(10)
+  @tweets = @user.get_recent_tweets
 
   erb :show_tweets
 end
--- app/models/twitter_user.rb
+++ app/models/twitter_user.rb
@@ -1,4 +1,14 @@
 class TwitterUser < ActiveRecord::Base
   # Remember to create a migration!
   has_many :tweets
+
+  def get_recent_tweets
+     self.tweets.order('tweet_created_at DESC').limit(10)
+  end
+
+  def tweets_stale?
+    return true if self.tweets.empty?
+
+    Time.now - self.updated_at > (15*60) # older than 15 mins
+  end
 end
