cc353c26d3de3feb3904d69d0b15fb2770e079c1
Apprentice <apprentice@dbc-chicago031.local>
2013-11-11 10:31:08 -0600

    MVP. Created models, migrations, and able to get and display 10 tweets

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,32 @@
+# Display a form to allow user to enter the twitter handle of the user to get tweets from
 get '/' do
-  # Look in app/views/index.erb
   erb :index
+end
+
+# Get the username the user entered and the last 10 tweets from that user
+post '/' do
+  twitter_handle = params[:twitter_username]
+  user = TwitterUser.find_or_create_by_name(:name => twitter_handle)
+
+  users_tweets = Twitter.user_timeline(twitter_handle)[0..9]
+
+  # tweet_text = []
+
+  users_tweets.each do |tweet|
+    # tweet_text << tweet[:attrs][:text]
+    user.tweets.find_or_create_by_tweet_created_at(:text => tweet[:attrs][:text], :tweet_created_at => tweet[:attrs][:created_at])
+    # puts tweet[:attrs][:text]
+    # puts
+  end
+
+
+   redirect to "/#{twitter_handle}"
+end
+
+# Display last 10 tweets of username
+get '/:username' do
+  @user = TwitterUser.find_by_name(params[:username])
+  @tweets = @user.tweets.limit(10)
+
+  erb :show_tweets
 end
